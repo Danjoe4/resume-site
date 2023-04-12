bf65ffbcde67c782347f1cba6c84ea774ebcce6e
Danjoe4 <djbroderick4@gmail.com>
2023-04-11 21:37:49 -0400

    desc_change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png',
+    title: 'Daniel Broderick | Developer',
+    description: 'My personal website and resume, give me a job!',
+    image: 'resume-site/public/images/android-chrome-512x512.png',
   }
 
   return (
